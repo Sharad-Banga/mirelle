2ea699947effe8a305f6bb647cd4136a5e5b80fa
Sharad Banga <sharadiit03@gmail.com>
2025-04-03 00:18:58 +0530

    adding collections

--- src/components/Fav.tsx
+++ src/components/Fav.tsx
@@ -14,36 +14,36 @@ const cards = [
     "id": 0,
     "i1": './images/f1.png',
     "i2":"./images/fi1.png",
-    "name":"Cove Knitted Shirt",
-    "price":"$ 125"
+    "name":"Warwick Waxed Jacket",
+    "price":"$364"
   },
   {
     "id": 1,
     "i1": "./images/f2.png",
     "i2":"./images/fi2.png",
-    "name":"bluuu Shirt",
-    "price":"$ 125"
+    "name":"Cove Knitted Shirt",
+    "price":"$179"
   },
   {
     "id": 2,
     "i1": "./images/f3.png",
     "i2":"./images/fi3.png",
-    "name":"whitee Shirt",
-    "price":"$ 125"
+    "name":"Whool Overshirt",
+    "price":"$297"
   },
   {
     "id": 3,
     "i1": "./images/f4.png",
     "i2":"./images/fi4.png",
-    "name":"green Shirt",
-    "price":"$ 125"
+    "name":"1796 Jumper",
+    "price":"$166"
   },
   {
     "id": 4,
     "i1": "./images/f5.png",
     "i2":"./images/fi5.png",
-    "name":"sss Shirt",
-    "price":"$ 125"
+    "name":"Bexley Jacket",
+    "price":"$364"
   },
 ];
 
@@ -95,9 +95,9 @@ export const Fav =({darki}:FavInter)=>{
     <div className={`${darki?"dark":""} `}>
 
 
-        <div className=" h-[100vh] bg-white dark:bg-black dark:text-white text-black flex-col flex justify-evenly" >
+        <div className=" h-[110vh] bg-white dark:bg-black dark:text-white text-black flex-col flex sm:justify-evenly" >
 
-        <div className="font-poppins text-3xl dark:text-white text-black flex justify-center">
+        <div className=" mt-12 sm:mb-16 font-poppins text-3xl dark:text-white text-black flex justify-center">
             Customer Favourites
         </div>
 
@@ -113,13 +113,13 @@ export const Fav =({darki}:FavInter)=>{
                   </div>
 
                     <div className=" flex-col sm:mt-10 bg-red h-[60vh]">
-                          <div className={` h-[85%] w-[80vw] sm:w-[22vw] bg-[url('./images/fi${x}.png')] bg-cover rounded-md`}>
+                          <div className={` h-[85%] w-[80vw] sm:w-[22vw] bg-[url('./images/fi${x}.png')] bg-cover rounded-md cursor-pointer hover:bg-[url('./images/f${x}.png')]`}>
                             
                           </div>
 
                           <div className="flex-col">
-                            <div className="flex justify-center">{cards[x-1].name}</div>
-                            <div className="flex justify-center">{cards[x-1].price}</div>
+                            <div className="flex justify-center font-poppins mt-5 text-[130%]">{cards[x-1].name}</div>
+                            <div className="flex justify-center text-black/55 dark:text-white/75 font-bold">{cards[x-1].price}</div>
                           </div>
                     </div>
                    
