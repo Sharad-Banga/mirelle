93eea03727d9b33b22d32bcb1361d8ba6daf1583
Sharad Banga <sharadiit03@gmail.com>
2025-04-03 12:48:49 +0530

    adding collections

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,12 +10,12 @@ export const Footer =({darki}:FooterInter)=>{
 
   return(
     <div className={`${darki?"dark":""}`}>
-        <div className="h-[60vh] bg-[#E0E0E0] dark:bg-[#181717] flex flex-col items-center justify-end">
+        <div className="h-[60vh] bg-[#E0E0E0] dark:bg-[#181717] flex flex-col items-center justify-end ">
 
               <div className="w-[90vw] sm:w-[80vw]  h-[60%] sm:h-[50%] dark:text-white
                border-t-2 border-black dark:border-white/55 flex justify-around sm:items-center font-poppins">
                   
-                  <div className="flex-col mt-2 sm:mt-0">
+                  <div className="flex-col mt-2 sm:mt-0 ml-5 sm:ml-0">
 
                     <a href=""><p className="font-semibold mb-3">SHOP</p></a>
                     <a href=""><p className="dark:text-white/65 font-light hover:font-normal dark:hover:text-white">MENS</p></a>
@@ -26,7 +26,7 @@ export const Footer =({darki}:FooterInter)=>{
 
                   </div>
 
-                  <div className="mt-2 sm:mt-0">
+                  <div className="mt-2 sm:mt-0 ml-7 sm:ml-0">
                     <a href=""><p className="font-semibold mb-3 ">HELP</p></a>
                     <a href=""><p className="dark:text-white/65 font-light hover:font-normal dark:hover:text-white">GET HELP</p></a>
                     <a href=""><p className="dark:text-white/65 font-light hover:font-normal dark:hover:text-white">ORDER STATUS</p></a>
@@ -35,7 +35,7 @@ export const Footer =({darki}:FooterInter)=>{
                     <a href=""><p className="dark:text-white/65 font-light hover:font-normal dark:hover:text-white">DAMAGE PRODUCT</p></a>
                   </div>
 
-                  <div className="mt-2 sm:mt-0">
+                  <div className="mt-2 sm:mt-0 ml-7 sm:ml-0">
                     <a href=""><p className="font-semibold mb-3">ABOUT</p></a>
                     <a href=""><p className="dark:text-white/65 font-light hover:font-normal dark:hover:text-white">NEWS</p></a>
                     <a href=""><p className="dark:text-white/65 font-light hover:font-normal dark:hover:text-white">CAREERS</p></a>
