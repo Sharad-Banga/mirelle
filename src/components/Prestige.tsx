0909549cf1258d9af955990372f80a065c7d5ab6
Sharad Banga <sharadiit03@gmail.com>
2025-04-03 18:39:35 +0530

    adding collections

--- src/components/Prestige.tsx
+++ src/components/Prestige.tsx
@@ -7,7 +7,7 @@ export const Prestige =({darki}:PrestigeInter) =>{
   return(
     <div  className={`${darki?"dark":""} `} >
 
-      <div className="h-[100vh] sm:h-[110vh] p-4 sm:p-0 flex-col flex justify-between border-t-2 border-black dark:border-white bg-[#E0E0E0] dark:bg-[#181717] dark:text-white border-b-2 ">
+      <div className="h-[90vh] sm:h-[110vh]  sm:p-0 flex-col flex justify-between border-t-2 border-black dark:border-white bg-[#E0E0E0] dark:bg-[#181717] dark:text-white border-b-2 ">
 
           <div className=" h-[15vh] mt-4 sm:mt-10 flex items-center justify-evenly gap-3 sm:gap-0">
 
@@ -42,7 +42,7 @@ export const Prestige =({darki}:PrestigeInter) =>{
                 </div>
             
           </div>
-          <div className=" h-[15vh] text-black/20 dark:text-white/15 font-rammetto  animate-pulse  flex-col flex justify-end text-[300%] ml-5 sm:text-[600%] ">
+          <div className=" h-[15vh] text-black/20 dark:text-white/15 font-rammetto  animate-pulse  flex items-end pb-0 text-[300%] ml-0  sm:text-[600%] leading-8 sm:leading-none ">
                 MIRELLE
           </div>
 
